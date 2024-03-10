93beab514d4af3700448cbbd6cc48e365ec3263c
GilmarFrancisc11 <161179559+GilmarFrancisc11@users.noreply.github.com>
2024-03-10 07:39:12 -0300

    Create iniciante.php

---
+++ iniciante.php
@@ -0,0 +1,10 @@
+<?php
+// Defina a variável $nome e atribua a string "Luzia" a ela
+$nome = "Luzia";
+
+// Defina a variável $idade e atribua um número inteiro à ela
+$idade = 25;
+
+// Imprima as variáveis $nome e $idade
+echo "Olá, meu nome é " . $nome . " e tenho " . $idade . " anos.";
+?>
